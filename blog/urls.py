aae406f815f6b39b97f0643492bd080e631b56d1
sujin <suuujin1225@gmail.com>
2020-05-11 04:55:58 +0900

    add HTML

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path    # 장고 함수
+from . import views             # 애플리케이션에서 사용할 모든 view 포함
+
+# 누군가가 http://127.0.0.1:8000/ 주소로 들어오면 views.post_list를 보여줌
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
