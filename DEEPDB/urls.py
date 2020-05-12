b9a9bf0a5802a9e118e83e409478bc437ea90666
yena <reve.yeans2@gmail.com>
2020-05-13 03:08:16 +0900

    폴더 옮김, 템플릿 완성, TemplateView 사용

--- DEEPDB/urls.py
+++ DEEPDB/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path    # 장고 함수
-from . import views             # 애플리케이션에서 사용할 모든 view 포함
+from django.urls import path
+from .views import QuizView, MainPageView, LearnSQLView
 
+# view에 정의된 함수를 불러 오는 곳
 # 누군가가 http://127.0.0.1:8000/ 주소로 들어오면 views.post_list를 보여줌
+
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
-    path('post/<int:pk>/', views.quiz_page, name='quiz_page'),
+    path('learn/', LearnSQLView.as_view(), name='learnSQL'),
+    path('quiz/', QuizView.as_view(), name='quiz'),
+    path('', MainPageView.as_view(), name='home'),
+    path('home/', MainPageView.as_view(), name='home'),
 ]
--- DEEPDB/views.py
+++ DEEPDB/views.py
@@ -1,10 +1,35 @@
+from django.http import HttpResponse
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+
 
 # Create your views here.
-def main_page(request):
-    return render(request, 'DEEPDB/main_page.html', {})
+# template_name을 정의하면 장고에서는 자동으로 앱 디렉토리의 templates 디렉토리를 참조한다고 한다
+class MainPageView(TemplateView):  # 메인페이지
+    template_name = 'main_page.html'
 
-def quiz_page(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'DEEPDB/quiz_page.html', {'post': post})
+    def get(self, request, *args, **kwargs):
+        ctx = {}  # 템플릿에 전달할 데이터
+        return self.render_to_response(ctx)
+
+
+class QuizView(TemplateView):  # 게시글 목록
+    template_name = 'quiz_page.html'
+    # queryset = Post.objects.all()  # 모든 퀴즈 문제
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}  # 템플릿에 전달할 데이터
+        return self.render_to_response(ctx)
+
+
+class LearnSQLView(TemplateView):  # SQL 학습 뷰
+    template_name = 'learn_sql.html'
+    
+    def get(self, request, *args, **kwargs):
+        ctx = {}  # 템플릿에 전달할 데이터
+        return self.render_to_response(ctx)
+
+# def quiz_page(request, pk):
+#   post = get_object_or_404(Post, pk=pk)
+#  return render(request, 'DEEPDB/quiz_page.html', {'post': post})
