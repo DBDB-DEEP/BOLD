54992606a81151346d0b9101bbafb74c6538336d
jooyena <Reve.yeans2@gmail.com>
2020-05-13 15:36:16 +0900

    model

--- DEEPDB/urls.py
+++ DEEPDB/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QuizView, MainPageView, LearnSQLView
+from .views import QuizView, MainPageView, LearnSQLView, QuizDetailView
 
 # view에 정의된 함수를 불러 오는 곳
 # 누군가가 http://127.0.0.1:8000/ 주소로 들어오면 views.post_list를 보여줌
@@ -7,6 +7,7 @@ from .views import QuizView, MainPageView, LearnSQLView
 urlpatterns = [
     path('learn/', LearnSQLView.as_view(), name='learnSQL'),
     path('quiz/', QuizView.as_view(), name='quiz'),
-    path('', MainPageView.as_view(), name='home'),
+    path('quiz/<problem_id>/', QuizDetailView.as_view(), name='quiz_detail'),
+    path('', MainPageView.as_view(), name='logo'),
     path('home/', MainPageView.as_view(), name='home'),
 ]
--- DEEPDB/views.py
+++ DEEPDB/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 
-from .models import Post
+from .models import Post, Problem
 from django.views.generic import TemplateView
 
 
@@ -16,8 +16,8 @@ class MainPageView(TemplateView):  # 메인페이지
 
 class QuizView(TemplateView):  # 게시글 목록
     template_name = 'quiz_page.html'
-    queryset = Post.objects.all()  # 모든 퀴즈 문제
-    pk_url_kwargs = 'article_id'
+    queryset = Problem.objects.all()  # 모든 퀴즈 문제
+    pk_url_kwargs = 'problem_id'
 
     def get_object(self, queryset=None):
         queryset = queryset or self.queryset
@@ -25,15 +25,34 @@ class QuizView(TemplateView):  # 게시글 목록
         return queryset.filter(pk=pk).first()  # pk로 검색된 데이터가 있다면 그 중 첫번째 데이터 없다면 None 반환
 
     def get(self, request, *args, **kwargs):
-        problem = self.get_object()
-        if not problem:
-            raise Http404('invalid id. plz check id')  # 검색된 데이터가 없다면 에러 발생
-
+        print(request.GET)
         ctx = {
             'view': self.__class__.__name__,  # 인스턴스 클래스 명
-            'data': problem
+            'problems': self.queryset  # 여기에 사용되었던 테그가 반환 되는 곳에서 이름 역활
         }  # 템플릿에 전달할 데이터
+        return self.render_to_response(ctx)
 
+
+class QuizDetailView(TemplateView):
+    template_name = 'quiz_detail.html'
+    queryset = Problem.objects.all()  # 모든 퀴즈 문제
+    pk_url_kwargs = 'problem_id'
+
+    def get_object(self, queryset=None):
+        queryset = queryset or self.queryset
+        pk = self.kwargs.get(self.pk_url_kwargs)
+        problem = queryset.filter(pk=pk).first()  # pk로 검색된 데이터가 있다면 그 중 첫번째 데이터 없다면 None 반환
+
+        if not problem:
+            raise Http404('invalid pk')
+        return problem
+
+    def get(self, request, *args, **kwargs):
+        problem = self.get_object()
+
+        ctx = {
+            'problem': problem  # 여기에 사용되었던 테그가 반환 되는 곳에서 이름 역활
+        }
         return self.render_to_response(ctx)
 
 
